797b92d53680280726e19f4ad8a426ad80c8f6cd
chany <lyhy0310@gmail.com>
2018-09-12 18:15:03 +0900

    [add] searchIcon style add

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from "styled-components";
-
+import { IconButton } from '../IconButton/IconButton';
 const arrowUpDown = require('../../assets/images/up-down.svg')
 
 const Wrapper = styled.div`
@@ -18,80 +18,45 @@ const FilterEl = styled.ul`
   height: 50px;
   margin: 0;
   padding-left: 0;
-`
-
-const FilterButtonStyle =`
-  font-family: 'open-arrow', Helvetica, sans-serif;
-  background: transparent;
-  position: relative;
-  padding: 10px;
-  border: 0; 
-
-`
+  & li {
+    display: flex;
+    font-family: SFCompactText;
+    font-size: 16px;
+    font-weight: 300;
+    line-height: 1.2;
+    text-align: left;
+    color: #b8b8b8;
   
-
-const FilterItemEl = styled.li`
-  display: flex;
-  font-family: SFCompactText;
-  font-size: 16px;
-  font-weight: 300;
-  line-height: 1.2;
-  text-align: left;
-  color: #b8b8b8;
-  
-  button {
-    ${FilterButtonStyle}
-    height: 50px;
-    margin-right: 15px;
-    &::before {
-    bottom: 0px;
-    left: 0;
-    right: 100%;
-    background: #222;
-    content: "";
-    box-sizing: border-box;
-    margin-left: 10px;
-    margin-right: 10px;
-   
-    position: absolute;
-    transition: all 0.2s ease-in-out;
-    z-index: 100;
-    height: 3px;
-  }
-  &:hover {
-    color: #222;
-    ::before {
-      right: 0;
+    button {
+      padding: 10px;
+      height: 50px;
+      margin-right: 15px;
+      &::before {
+      bottom: 0px;
+      left: 0;
+      right: 100%;
+      background: #222;
+      content: "";
+      box-sizing: border-box;
+      margin-left: 10px;
+      margin-right: 10px;
+    
+      position: absolute;
+      transition: all 0.2s ease-in-out;
+      z-index: 100;
+      height: 3px;
+    }
+    &:hover {
+      color: #222;
+      ::before {
+        right: 0;
+      }
     }
   }
 }
 `
 
-const IconButtonEl = styled.button`
-  ${FilterButtonStyle}
-  width: 50px;
-  height: 50px;
-  & img {
-    max-width: 100%;
-  }
-`
-
-const FilterItem = ({buttonText}) => {
-  return (
-    <FilterItemEl>
-      <button>{buttonText}</button>
-    </FilterItemEl>
-  );
-};
-
-const IconButton = ({iconSrc})=>{
-  return (
-    <IconButtonEl>
-      <img src={iconSrc} alt="sort button"/>
-    </IconButtonEl>
-  )
-}
-
+const sortSize = 40;
 
 class Filter extends Component {
   render() {
@@ -99,13 +64,12 @@ class Filter extends Component {
       <Wrapper>
         <FilterEl>
           {this.props.filterList.map(({id, text})=>(
-            <FilterItem 
-              key={id}
-              buttonText={text}
-            />
+            <li key={id}>
+              <button>{text}</button>
+            </li>
           ))} 
         </FilterEl>
-        <IconButton iconSrc={arrowUpDown}/>
+        <IconButton iconSrc={arrowUpDown} size={sortSize}/>
       </Wrapper>
     );
   }
--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import styled from "styled-components";
 
+const searchIcon = require('../../assets/images/search-icon.svg')
+
+const FormWrapper = styled.form `
+   position: relative;
+   box-sizing: border-box;
+   padding-left: 10px;
+   padding-right: 10px;
+`
+
 const StyledInput = styled.input`
-  position: relative;
+ 
   width: 345px;
   height: 35px;
   border-radius: 5px;
@@ -13,13 +22,27 @@ const StyledInput = styled.input`
     color: #d8d8d8
   }
 `
+const SearchButton = styled.button`
+  position: absolute;
+  left: 10px;
+  width: 35px;
+  height: 35px;
+  
+  & img {
+    max-width: 100%;
+  }
+`
+
 
 class SearchInput extends Component {
   render() {
     return (
-      <form>
+      <FormWrapper>
+        <SearchButton>
+          <img src={searchIcon} alt=""/>
+        </SearchButton>
         <StyledInput type="text" placeholder="Search for tasks"/>
-      </form>
+      </FormWrapper>
     );
   }
 }
