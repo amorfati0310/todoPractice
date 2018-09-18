4585e6c4dcea43c5b8c43367d558c7868dd5e5a7
chany <lyhy0310@gmail.com>
2018-09-18 17:30:32 +0900

    [add] font  적용

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import registerServiceWorker from './registerServiceWorker';
-
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
